c77abb55cbca8869715689af00f8042102d1a7d8
Alvaro de Francisco <alfrasan@gmail.com>
2019-05-02 19:54:13 +0200

    Checking first push in GitKraken |
    Using this time GitKraken instead of SourceTree. In order to get used to this software, I updated the .gitignore file and created the basics of the Main Class of the videogame

---
+++ src/voidgame/VoidGameMain.java
@@ -0,0 +1,15 @@
+package voidgame;
+
+public class VoidGameMain {
+	
+	/**
+	 * Main Class for executing the videogame
+	 * @param args
+	 * @author Alvaro de Francisco Sanchez
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
